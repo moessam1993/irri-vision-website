604fe1215ee19349699056d785600299ea4ac1aa
YousefGmal <49760945+YousefGmal@users.noreply.github.com>
2025-05-03 02:27:12 +0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import { contactRouter } from './component/contactUs/contact.router';
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { contactRouter } from "./component/contactUs/contact.router";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
-app.use('/api', contactRouter);
+app.use("/api", contactRouter);
 
 export default app;
